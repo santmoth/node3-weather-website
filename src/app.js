620c1c7be641001ce46570af26113ac02599010e
santhosh <santhoshsharma29@gmail.com>
2021-04-24 22:36:38 +0530

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const  port = process.env.PORT || 3000
 
 //Define Paths foe express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -96,4 +97,7 @@ app.get('*', (req, res) => {
     })
 })
 
+app.listen(port , () => {
+    console.log('server is up on port ' + port)
+})
  
